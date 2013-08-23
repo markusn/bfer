d280b85191a528740682862e811644f8ae15fc69
Markus Näsman <markus@botten.org>
2013-08-24 01:02:47 +0200

    Removed unused define

--- src/bfer.erl
+++ src/bfer.erl
@@ -92,30 +92,6 @@ compile_helloworld_test() ->
   {ok, BFCode}  = file:read_file(TestDir ++ "helloworld.bf"),
   ?assertEqual(binary_to_list(LLCode), compile(binary_to_list(BFCode))).
 
--define(HELLOWORLD,
-        "+++++ +++++             initialize counter (cell #0) to 10"
-        "[                       use loop to set the next four cells to"
-        "70/100/30/10"
-        "> +++++ ++              add  7 to cell #1"
-        "> +++++ +++++           add 10 to cell #2"
-        "> +++                   add  3 to cell #3"
-        "> +                     add  1 to cell #4"
-        "<<<< -                  decrement counter (cell #0)"
-        "]"
-        "> ++ .                  print 'H'"
-        "> + .                   print 'e'"
-        "+++++ ++ .              print 'l'"
-        ".                       print 'l'"
-        "+++ .                   print 'o'"
-        "> ++ .                  print ' '"
-        "<< +++++ +++++ +++++ .  print 'W'"
-        "> .                     print 'o'"
-        "+++ .                   print 'r'"
-        "----- - .               print 'l'"
-        "----- --- .             print 'd'"
-        "> + .                   print '!'"
-        "> .                     print '\n'").
-
 %%-endif.
 %%% Local Variables:
 %%% allout-layout: t
